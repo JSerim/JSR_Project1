457d980bb5aa919b77a012ea54623f349c191037
JSerim <fla0723@naver.com>
2022-12-06 09:34:08 +0900

    최종

--- admin/inc/login_check.php
+++ admin/inc/login_check.php
@@ -3,7 +3,7 @@ if(!$s_idx){
     echo "
     <script type=\"text/javascript\">
         alert(\"로그인 사용자만 접근할 수 있습니다.\");
-        location.href=\"http://localhost/JSR_Project1/login/login.php\";
+        location.href=\"http://fla0723.dothome.co.kr/JSR_Project1/login/login.php\";
         </script>
     ";
     exit;
